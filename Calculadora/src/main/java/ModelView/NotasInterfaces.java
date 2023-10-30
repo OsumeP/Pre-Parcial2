14584bdae8010ae0fc389e422758dd43d6fa082f
Juan David Patarroyo Sánchez <juandavid.patarroyosanchez@gmail.com>
2023-10-30 17:06:47 -0500

    Adicion notas

---
+++ Calculadora/src/main/java/ModelView/NotasInterfaces.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModelView;
+
+/**
+ *
+ * @author Juan David Patarroyo
+ */
+public class NotasInterfaces {
+    /*Para declarar una interfaz:
+        public interface NombreInterfaz{
+            public boolean NombreFunción(); NO tienen cuerpo, son abstractas.
+        }
+        
+    Para implementar una interfaz:
+    public class Line implements Relation{
+        public boolean NombreFunción(){
+            Código del método abstracto
+        }
+    }
+    
+    Para declarar una clase abstracta:
+        public abstract class LivingThing{
+            public abstract void walk();
+        }
+    super(atributos)
+    */
+}
